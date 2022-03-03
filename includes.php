477edfbcf3b9bcaa9faf32e1833688bd9d3bc592
wzsolt <wzsolt@yton.hu>
2022-03-03 07:02:01 +0100

    memcache key fix

--- includes.php
+++ includes.php
@@ -1,9 +1,9 @@
 <?php
 const DOC_ROOT = __DIR__ . '/';
 
+include_once DOC_ROOT . 'config/config.php';
 include_once DOC_ROOT . 'shared/shared.includes.php';
 include_once DOC_ROOT . 'config/constants.php';
-include_once DOC_ROOT . 'config/config.php';
 
 // Composer auto loader
 require_once DOC_ROOT . 'vendor/autoload.php';
