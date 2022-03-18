94f8ba02b2f7dc8f9518c49867f1bd9ee760a4bf
wzsolt <wzsolt@yton.hu>
2022-03-18 14:20:59 +0100

    fix

--- web/public/tools/index.php
+++ web/public/tools/index.php
@@ -4,19 +4,6 @@ require_once(__DIR__ . '/../web.includes.php');
 $fw = new router();
 $fw->init();
 
-$config = $fw->getHostConfig();
-
-$publicDomain = rtrim($config['publicSite'], '/') . '/';
-
-$publicDomain = false;
-if(!Empty($fw->getHostConfig()['publicSite'])){
-    $publicDomain = rtrim($fw->getHostConfig()['publicSite'], '/') . '/';
-}
-
-
-d(($publicDomain ?: $fw->domain));
-dd($config);
-
 include_once('header.php');
 ?>
 
